43387aa6c63c1905e1e7e70bec9f1dd9782c87e4
edubusluc <edubusluc@alum.us.es>
2023-02-25 19:30:52 +0100

    First steps in the creation of the Student entity.

---
+++ src/main/java/acme/roles/Student.java
@@ -0,0 +1,39 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.*;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Student extends AbstractRole {
+	
+	protected static final long serialVersionUID = 1L;
+	
+	@NotBlank
+	@Length(max = 75)
+	protected String statement;
+	
+	@NotBlank
+	@Length(max = 100)
+	protected String strongFeatures;
+	
+	@NotBlank
+	@Length(max = 100)
+	protected String weakFeatures;
+	
+	@URL
+	protected String moreInfo;
+	
+	
+	
+
+}
