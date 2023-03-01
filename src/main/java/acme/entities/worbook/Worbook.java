9d7fddc0a35ff58d7a1d280a1b29520b0a3e26ce
edubusluc <edubusluc@alum.us.es>
2023-03-01 14:13:30 +0100

    fix: Worbook Entity

--- src/main/java/acme/entities/worbook/Worbook.java
+++ src/main/java/acme/entities/worbook/Worbook.java
@@ -3,6 +3,7 @@ package acme.entities.worbook;
 
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -12,7 +13,12 @@ import org.hibernate.validator.constraints.Length;
 
 import acme.entities.activity.Activity;
 import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Worbook extends AbstractEntity {
 
 	protected static final long	serialVersionUID	= 1L;
