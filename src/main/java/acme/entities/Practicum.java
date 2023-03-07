72c2ac8758a8f0b9dd1d5cbc964a5d32a6d1de82
Juan <juanmc015@gmail.com>
2023-03-07 15:52:48 +0100

    Relation company-practicum

--- src/main/java/acme/entities/Practicum.java
+++ src/main/java/acme/entities/Practicum.java
@@ -3,12 +3,16 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
 import acme.framework.data.AbstractEntity;
+import acme.roles.Company;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -37,4 +41,12 @@ public class Practicum extends AbstractEntity {
 	@NotBlank
 	@Length(max = 101)
 	protected String			goals;
+
+	// Derived attributes
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Company			company;
+
 }
