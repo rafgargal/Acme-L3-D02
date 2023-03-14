9705ef8d8f60d8196957624b8b94849d76bf1e27
Juan <juanmc015@gmail.com>
2023-03-14 16:19:06 +0100

    created dashboard

---
+++ src/main/java/acme/forms/PracticumDashboard.java
@@ -0,0 +1,18 @@
+
+package acme.forms;
+
+import acme.framework.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PracticumDashboard extends AbstractForm {
+
+	//Serialisation Identifier
+
+	protected static final long serialVersionUID = 1L;
+
+	//Atributes
+
+}
